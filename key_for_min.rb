71d2527a7a8158b871eba424f464bd4e27d9cd1c
Jouse then <jthen93@gmail.com>
2021-02-18 00:48:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,15 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-
+  if is_empty?(name_hash)
+    return nil
+  else
+    lowest_value = {}
+    name_hash.each do |key, value|
+      if value < lowest_value || lowest_value == nil
+        lowest_value = value
+      end
+    end
+    
+    return v
 end
