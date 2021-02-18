678ada3b48431688324d61055bf7375a24435ea0
Jouse then <jthen93@gmail.com>
2021-02-18 00:49:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -6,11 +6,14 @@ def key_for_min_value(name_hash)
     return nil
   else
     lowest_value = nil
+    lowest_key = nil
+    
     name_hash.each do |key, value|
       if value < lowest_value || lowest_value == nil
         lowest_value = value
+        lowest_key = key
       end
     end
-    return v
+    return lowest_key
   end
 end
